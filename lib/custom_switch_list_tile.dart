51c28f48500a412f2f490a8adf65d02f861faa7c
tomazjr <tomazcostasoares@gmail.com>
2023-05-10 12:34:55 -0300

    Created custom checkbox and switch list tile widgets

---
+++ lib/custom_switch_list_tile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomSwitchListTile extends StatelessWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final Widget? title;
+  final Color? tileColor;
+  const CustomSwitchListTile({
+    super.key,
+    required this.value,
+    required this.onChanged,
+    this.title,
+    this.tileColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    /// A SwitchListTile requires a Material widget ancestor in the tree
+    /// to paint itself on, which is typically provided by the app's Scaffold.
+    return Material(
+      child: SizedBox(
+        width: 300,
+        child: SwitchListTile(
+          value: value,
+          onChanged: (bool newValue) {
+            onChanged(newValue);
+          },
+          title: title,
+          tileColor: tileColor,
+        ),
+      ),
+    );
+  }
+}
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_examples/custom_checkbox_list_tile.dart';
 import 'package:widgets_examples/custom_dropdown_button_form_field.dart';
+import 'package:widgets_examples/custom_switch_list_tile.dart';
 import 'package:widgets_examples/custom_text_button.dart';
 import 'package:widgets_examples/custom_text_form_field.dart';
 
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget {
   const SecondScreen({super.key});
 
   @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  List<String>? items = ['João', 'Maria', 'Ana', 'José'];
+  TextEditingController birthdayController = TextEditingController();
+  String? userName = '';
+  bool? isStudent = false;
+  bool? isWorking = false;
+  @override
   Widget build(BuildContext context) {
-    List<String>? items = ['João', 'Maria', 'Ana', 'José'];
-    TextEditingController birthdayController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Second Screen'),
@@ -21,10 +31,12 @@ class SecondScreen extends StatelessWidget {
               const SizedBox(
                 height: 25,
               ),
+
+              /// Pode ser usado apenas em uma tela Stateless
               CustomDropdownButtonFormField(
                 hint: const Text('Selecione o Usuário:'),
                 prefixIcon: const Icon(Icons.person),
-                items: items.map<DropdownMenuItem<String>>(
+                items: items?.map<DropdownMenuItem<String>>(
                   (String? value) {
                     return DropdownMenuItem<String>(
                       value: value,
@@ -32,28 +44,69 @@ class SecondScreen extends StatelessWidget {
                     );
                   },
                 ).toList(),
-                onChanged: (dynamic value) {},
+                onChanged: (dynamic value) {
+                  userName = value;
+                },
               ),
               const SizedBox(
                 height: 25,
               ),
+
+              /// Pode ser usado apenas em uma tela Stateless
               CustomTextFormField(
                 controller: birthdayController,
               ),
               const SizedBox(
                 height: 25,
               ),
-              CustomTextButton(
-                onPressed: () {},
-                child: const Text('Confirmar'),
+              CustomCheckboxListTile(
+                value: isStudent,
+                onChanged: (bool? newValue) {
+                  setState(() {
+                    isStudent = newValue;
+                  });
+                },
+                title: const Text('É Estudante?'),
+                // tileColor: Colors.green,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              CustomSwitchListTile(
+                value: isWorking!,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    isWorking = newValue;
+                  });
+                },
+                title: const Text('Está Trabalhando?'),
+                // tileColor: Colors.green,
               ),
               const SizedBox(
                 height: 50,
+              ),
+              CustomTextButton(
+                onPressed: () {
+                  print('UserName: $userName');
+                  print('Nascimento: ${birthdayController.text}');
+                  print('É Estudante: $isStudent');
+                  print('Está Trabalhando: $isWorking');
+                },
+                child: const Text('Confirmar'),
+              ),
+              const SizedBox(
+                height: 25,
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    birthdayController.dispose();
+    super.dispose();
   }
 }
